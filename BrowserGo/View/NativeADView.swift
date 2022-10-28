46af35be0160c6899a281eae30ec2001db3f8bba
Nico <nicoyang.mason@gmail.com>
2022-10-28 14:31:01 +0800

    正式配置

---
+++ BrowserGo/View/NativeADView.swift
@@ -0,0 +1,20 @@
+//
+//  NativeADView.swift
+//  BrowserGo
+//
+//  Created by yangjian on 2022/10/27.
+//
+
+import UIKit
+
+class NativeADView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
